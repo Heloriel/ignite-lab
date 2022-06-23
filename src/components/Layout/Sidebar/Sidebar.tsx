0a72b29f5198c9e6c2eeb9114ae1524e0fca6926
Heloriel <andradematheus.dev@gmail.com>
2022-06-23 15:02:16 -0300

    Feat: Functional sidebar

--- src/components/Layout/Sidebar/Sidebar.tsx
+++ src/components/Layout/Sidebar/Sidebar.tsx
@@ -1,19 +1,54 @@
+import { gql, useQuery } from "@apollo/client";
 import { Lesson } from "../../Lesson/Lesson";
 
-export const Sidebar = () => {
-    return (
-        <div className="flex flex-col w-[348px] bg-gray-700 border-l border-gray-600 p-6">
-            <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
-                Cronograma das Aulas
-            </span>
-            <div className="flex flex-col gap-8">
-                <Lesson />
-                <Lesson />
-                <Lesson />
-                <Lesson />
-                <Lesson />
-                <Lesson />
-            </div>
-        </div>
-    );
+interface Lessons {
+  id: string;
+  title: string;
+  slug: string;
+  description: string;
+  availableAt: string;
+  teacher: {
+    id: string;
+    name: string;
+  };
+  lessonType: "live" | "class";
 }
+
+export const Sidebar = () => {
+  const GET_LESSONS_QUERY = gql`
+    query LessonsQuery {
+      lessons(orderBy: availableAt_ASC) {
+        id
+        lessonType
+        slug
+        title
+        description
+        availableAt
+      }
+    }
+  `;
+
+  const { data } = useQuery<{ lessons: Lessons[] }>(GET_LESSONS_QUERY);
+
+  return (
+    <div className="flex flex-col w-[348px] bg-gray-700 border-l border-gray-600 p-6">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+        Cronograma das Aulas
+      </span>
+      <div className="flex flex-col gap-8">
+        {data?.lessons.map((lesson) => {
+          return (
+            <Lesson
+              key={lesson.id}
+              availableAt={lesson.availableAt}
+              description={lesson.description}
+              slug={lesson.slug}
+              lessonType={lesson.lessonType}
+              title={lesson.title}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
--- src/components/Lesson/Lesson.tsx
+++ src/components/Lesson/Lesson.tsx
@@ -1,28 +1,44 @@
-import { CheckCircle } from "phosphor-react";
+import { format, isPast } from "date-fns";
+import { CheckCircle, Lock } from "phosphor-react";
 
-export const Lesson = () => {
-    return (
-        <a href="#">
-            <span className="block mb-2 text-gray-300">
-                Domingo • 20 de junho • 19h00
-            </span>
-
-            <div className="flex flex-col w-full justify-center border border-gray-500 p-4 rounded">
-
-                <header className="flex w-full items-center justify-between mb-4">
-                    <span className="flex flex-row items-center text-sm text-blue-500 font-medium">
-                        <CheckCircle size={24} className="mr-[10.5px]" />
-                        Conteúdo Liberado
-                    </span>
-                    <span className="text-xs font-bold py-[2px] px-2 border border-green-300 rounded">
-                        AO VIVO
-                    </span>
-                </header>
-
-                <strong className="">
-                    Abertura do evento ignite.
-                </strong>
-            </div>
-        </a>
-    );
+interface LessonProps {
+  title: string;
+  slug: string;
+  description: string;
+  availableAt: string;
+  lessonType: "live" | "class";
 }
+
+export const Lesson = (props: LessonProps) => {
+  const formatedDate = new Date(props.availableAt);
+  const isLessonAvailableAt = isPast(formatedDate);
+
+  return (
+    <a href={props.slug}>
+      <span className="block mb-2 text-gray-300">
+        {format(formatedDate, "eeee' • 'dd' de 'LLLL' • 'HH'h'mm")}
+      </span>
+
+      <div className="flex flex-col w-full justify-center border border-gray-500 p-4 rounded">
+        <header className="flex w-full items-center justify-between mb-4">
+          {isLessonAvailableAt ? (
+            <span className="flex flex-row items-center text-sm text-blue-500 font-medium">
+              <CheckCircle size={24} className="mr-[10.5px]" />
+              Conteúdo Liberado
+            </span>
+          ) : (
+            <span className="flex flex-row items-center text-sm text-orange-500 font-medium">
+              <Lock size={24} className="mr-[10.5px]" />
+              Em Breve
+            </span>
+          )}
+          <span className="text-xs font-bold py-[2px] px-2 border border-green-300 rounded">
+            {props.lessonType === "live" ? "AO VIVO" : "AULA PRÁTICA"}
+          </span>
+        </header>
+
+        <strong className="">{props.title}</strong>
+      </div>
+    </a>
+  );
+};
